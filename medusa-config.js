eb3362b5f78a20a159b04ce88fb12d5b0247f675
Pedro Lages <plagesribeiro@gmail.com>
2023-03-10 09:49:42 -0300

    stripe

--- medusa-config.js
+++ medusa-config.js
@@ -43,15 +43,14 @@ const STRIPE_WEBHOOK_SECRET = process.env.STRIPE_WEBHOOK_SECRET || '';
 const plugins = [
 	`medusa-fulfillment-manual`,
 	`medusa-payment-manual`,
-	// Uncomment to add Stripe support.
-	// You can create a Stripe account via: https://stripe.com
-	// {
-	//   resolve: `medusa-payment-stripe`,
-	//   options: {
-	//     api_key: STRIPE_API_KEY,
-	//     webhook_secret: STRIPE_WEBHOOK_SECRET,
-	//   },
-	// },
+
+	{
+		resolve: `medusa-payment-stripe`,
+		options: {
+			api_key: process.env.STRIPE_API_KEY,
+			webhook_secret: process.env.STRIPE_WEBHOOK_SECRET
+		}
+	},
 	{
 		resolve: `medusa-file-spaces`,
 		options: {
